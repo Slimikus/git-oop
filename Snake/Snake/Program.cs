6100555157a6408989b2ea894ef2488263b97aa5
Slimikus <slimikus@gmail.com>
2016-01-28 13:48:49 +0300

    Добавление строк для создание линий

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,43 +16,8 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Point p3 = new Point(6, 4, '%');
-            p3.Draw();
-
-            Point p4 = new Point(9, 13, '@');
-            p4.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            /*numList.RemoveAt(0);*/
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-
-            List<int> charList = new List<int>();
-            charList.Add('&');
-            charList.Add('#');
-            charList.Add('@');
-
-            foreach (char e in charList)
-            {
-                Console.WriteLine(e);
-            }
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
 
             Console.ReadLine();
         }
