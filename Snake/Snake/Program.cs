162e6723dbb95917736c51134c4083988ea143ff
Slimikus <slimikus@gmail.com>
2016-01-29 14:56:36 +0300

    удалён не нужный кусок кода

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -26,11 +26,5 @@ namespace Snake
 
             Console.ReadLine();
         }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
-    }
+   }
 }
