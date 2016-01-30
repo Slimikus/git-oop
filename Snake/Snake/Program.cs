e256a2f1d990700f36a9c0af573df04055543c7d
Slimikus-Home-Win <slimikus@gmail.com>
2016-01-30 22:26:45 +0300

    перенос остроение стен в класс Walls + конец игры при врезание в стену или хвост

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,15 +14,9 @@ namespace Snake
             Console.SetWindowSize(1, 1);
             Console.SetBufferSize(80, 25);
             Console.SetWindowSize(80, 25);
-            // Отрисовка рамочки
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            upLine.Draw();
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            downLine.Draw();
-            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
-            leftLine.Draw();
-            VerticalLine rightLine = new VerticalLine(78, 0, 24, '+');
-            rightLine.Draw();
+
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             //отрисовка точек
             Point p = new Point(4, 5, '*');
@@ -35,6 +29,10 @@ namespace Snake
             
             while(true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
